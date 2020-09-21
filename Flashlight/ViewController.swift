699c7665eff39992ae5a780a110fe13d29f6e2c9
Qian <gokinjolno112358@gmail.com>
2020-09-21 20:45:24 +0800

    First Commit

--- Flashlight/ViewController.swift
+++ Flashlight/ViewController.swift
@@ -6,14 +6,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var isOn = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var mainView: UIView!
+    
+    @IBOutlet weak var switchButton: UIButton!
+    
+    @IBAction func eventHandler(){
+        if isOn{
+            lightOff()
+        } else {
+            lightOn()
+        }
+    }
+    
+    func lightOn(){
+        
+        mainView.backgroundColor = .white
+        switchButton.setTitle("Off", for:.normal)
+        switchButton.setTitleColor(.black, for:.normal)
+        isOn = true
+        if let device = AVCaptureDevice.default(for: .video){
+            if device.hasTorch{
+                do{
+                    try device.lockForConfiguration()
+                    device.torchMode = .on
+                } catch{
+                    print("Touch could not be used")
+                }
+            } else {
+                print("Torch is not available")
+            }
+        }
+    }
+    
+    func lightOff(){
+        mainView.backgroundColor = .black
+        switchButton.setTitle("On", for:.normal)
+        switchButton.setTitleColor(.white, for:.normal)
+        isOn = false
+        if let device = AVCaptureDevice.default(for: .video){
+            if device.hasTorch{
+                do{
+                    try device.lockForConfiguration()
+                    device.torchMode = .off
+                } catch{
+                    print("Touch could not be used")
+                }
+            } else {
+                print("Torch is not available")
+            }
+        }
+    }
 }
 
